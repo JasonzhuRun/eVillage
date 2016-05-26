2c8c0051fdcecb3618511a3c6d00a469524b35ac
玄山 <yixuan.gyx@alibaba-inc.com>
2016-05-26 22:37:22 +0800

    修改gitignore

--- app/src/main/java/cn/deepai/evillage/controller/activity/MainTabActivity.java
+++ app/src/main/java/cn/deepai/evillage/controller/activity/MainTabActivity.java
@@ -19,6 +19,8 @@ public class MainTabActivity extends BaseActivity {
 
     private FragmentTabHost mTabHost;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
