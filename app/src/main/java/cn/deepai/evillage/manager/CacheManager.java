dc54560659b668e47fe192c6d217835429ff089d
玄山 <yixuan.gyx@alibaba-inc.com>
2016-05-30 21:27:41 +0800

    新建缓存系统

---
+++ app/src/main/java/cn/deepai/evillage/manager/CacheManager.java
@@ -0,0 +1,33 @@
+package cn.deepai.evillage.manager;
+
+import java.util.List;
+
+import cn.deepai.evillage.model.Villager;
+import cn.deepai.evillage.utils.FileUtil;
+
+/**
+ * Created by GaoYixuan on 16/5/30.
+ */
+public class CacheManager {
+
+    public static final int CACHE_DETAIL = 0x01;   // 贫困户详情缓存
+
+
+
+    private static CacheManager instance;
+    private CacheManager() {
+        cacheFileDir = FileUtil.getPrivateDir();
+    }
+    static {
+        instance = new CacheManager();
+    }
+
+    private List<Villager> villagers;
+    private String cacheFileDir;
+
+    public CacheManager getInstance() {
+        return instance;
+    }
+
+
+}
