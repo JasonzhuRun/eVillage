1b4e318725511a71b2111d78d9ae0369b35ca973
玄山 <yixuan.gyx@alibaba-inc.com>
2016-06-06 19:47:07 +0800

    request

---
+++ app/src/main/java/cn/deepai/evillage/model/EVDiskRequest.java
@@ -0,0 +1,31 @@
+package cn.deepai.evillage.model;
+
+import cn.deepai.evillage.EVApplication;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+
+/**
+ * @author GaoYixuan
+ */
+public abstract class EVDiskRequest {
+
+    private static final MediaType MEDIA_TYPE = MediaType.parse("application/json; charset=utf-8");
+    private static final String mURL = "http://"+
+            EVApplication.getHost()+"/zy_fp/api/{model}/{version}/interface";
+    private static OkHttpClient client = new OkHttpClient();
+
+    public void request(String action,String jsonData,Callback callback) {
+        //todo  将请求头也放在这里封装
+        RequestBody body = RequestBody.create(MEDIA_TYPE, jsonData);
+
+        Request request = new Request.Builder()
+                .url(mURL+action)
+                .post(body)
+                .build();
+
+        client.newCall(request).enqueue(callback);
+    }
+}
